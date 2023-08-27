370d331bd9ebc589089f438353e2b32ac40bda2d
Ian <ian.odhiambo@student.moringaschool.com>
2023-08-27 11:00:59 +0300

    Full names of customers

--- customer.py
+++ customer.py
@@ -1,7 +1,10 @@
 class Customer :
+
+    all_customers = []
     def __init__(self , given_name , family_name):
         self.given_name = given_name
         self.family_name = family_name
+        Customer.all_customers.append(self) 
 
     def change_given_name (self , new_given_name):
         self.given_name = new_given_name
@@ -18,13 +21,13 @@ class Customer :
     def full_name (self):
         return f"{self .given_name} {self.family_name} "
     
-
+    @classmethod
     def all(cls):
 
-        return all_customer
+        return cls.all_customers
     
 customer1 = Customer("George", "Washington")
 
-all_customer = Customer.all()
-for customer in all_customer:
+all_customers = Customer.all()
+for customer in all_customers:
     print(customer.full_name())
