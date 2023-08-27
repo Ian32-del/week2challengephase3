a2ab9f75acc769da2eb933a0a43cc0966e0ae65b
Ian <ian.odhiambo@student.moringaschool.com>
2023-08-27 12:50:53 +0300

    Review

--- review.py
+++ review.py
@@ -1,12 +1,22 @@
 class Review :
     all_reviews = []
-    def __init__(self , customer , restuarant , rating ):
+
+    def __init__(self , customer , restaurant , rating ):
         self.customer = customer 
-        self.restuarant = restuarant
+        self.restaurant = restaurant
         self.rating = rating 
         Review.all_reviews.append(self)
 
     def rating(self):
         return self.rating
     
+    @classmethod
+    def all (cls):
+        return cls.all_reviews
     
+name = Review("Jane","All You Can Eat",8)
+
+all_reviews = Review.all()
+
+for review in all_reviews:
+    print(f"Customer:{review.customer}, Restaurant:{review.restaurant} ,Rating:{review.rating}")
