62f624b1b17f92bf0fa841ba817e48f7d9d6e596
Ian <ian.odhiambo@student.moringaschool.com>
2023-08-27 11:56:23 +0300

    Restuarant

---
+++ restuarant.py
@@ -0,0 +1,9 @@
+class Restuarant :
+    def __init__(self , name):
+        self._name = name
+
+    def name (self):
+        return self._name
+    
+recipe = Restuarant("Eat Here")
+print(recipe.name())
